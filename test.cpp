382569f4a2a538130b98c19333d258c42b88daa3
IK25268 <162690760+IK25268@users.noreply.github.com>
2024-05-30 14:04:23 +0700

    Dijkstra1.5

--- test.cpp
+++ test.cpp
@@ -1,15 +1,104 @@
 #include "pch.h"
-#include "../DijkstraFTIOOP3Block/RWFile.hpp"
-#include "../DijkstraFTIOOP3Block/Dijkstra.hpp"
-#include "../DijkstraFTIOOP3Block/RWFile.cpp"
-#include "../DijkstraFTIOOP3Block/Dijkstra.cpp"
-#include "../DijkstraFTIOOP3Block/Graph.cpp"
-#include "../DijkstraFTIOOP3Block/Graph.hpp"
+#include "../GPSFTIOOP4Block/Memory.hpp"
+#include "../GPSFTIOOP4Block/RWFile.hpp"
+#include "../GPSFTIOOP4Block/Handler.hpp"
+#include "../GPSFTIOOP4Block/Memory.cpp"
+#include "../GPSFTIOOP4Block/RWFile.cpp"
+#include "../GPSFTIOOP4Block/Handler.cpp"
+#include "../GPSFTIOOP4Block/Point.cpp"
+#include "../GPSFTIOOP4Block/Point.hpp"
+#include "../GPSFTIOOP4Block/DistribSpd.hpp"
+#include "../GPSFTIOOP4Block/DistribSpd.cpp"
 
-TEST(TestRoutes, TestName) {
-	Graph graph(RWFile::ReadFile("input.txt"));
-	std::string routeKT = Dijkstra::CalcRoute(graph, "Krasnoyarsk", "Toronto");
-	std::string routeTO = Dijkstra::CalcRoute(graph, "Toronto", "Omsk");
-	ASSERT_STREQ(routeKT.data(), "{Krasnoyarsk, Kiev, Omsk, Toronto} - 26");
-	ASSERT_STREQ(routeTO.data(), "{Toronto, Krasnoyarsk, Kiev, Omsk} - 17");
+TEST(TestCalcValues, TestDistance) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetDistanñe()).data(), "11808.739638");
+}
+
+TEST(TestCalcValues, TestAverSpeed) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetAverSpeed()).data(), "2.986530");
+}
+
+TEST(TestCalcValues, TestMoutTime) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetMoutTime()).data(), "3899");
+}
+
+TEST(TestCalcValues, TestStopTime) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetStopTime()).data(), "55");
+}
+
+TEST(TestCalcValues, TestAverMoutSpeed) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetAverMoutSpeed()).data(), "3.028659");
+}
+
+TEST(TestCalcValues, TestMaxSpeed) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetMaxSpeed()).data(), "10.600183");
+}
+
+TEST(TestCalcValues, TestMinAltit) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetMinAltit()).data(), "544.372799");
+}
+
+TEST(TestCalcValues, TestMaxAltit) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetMaxAltit()).data(), "605.332799");
+}
+
+TEST(TestCalcValues, TestSummAscend) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetSummAscend()).data(), "508.711199");
+}
+
+TEST(TestCalcValues, TestSummDescend) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	Handler handler;
+	handler.CalcValues(memory, 0.3);
+	ASSERT_STREQ(std::to_string(handler.GetSummDescend()).data(), "495.299999");
+}
+
+TEST(TestCalcValues, TestDistribSpd) {
+	
+	Memory memory = RWFile::ReadFile("input.txt");
+	memory.AddSpdRange({ 0.0, 4.0 });
+	memory.AddSpdRange({ 5.0, 7.0 });
+	memory.AddSpdRange({ 8.0, 20.0 });
+	DistribSpd distribSpd;
+	distribSpd.CalcRngSpd(memory);
+	ASSERT_EQ(distribSpd.GetTimes()[0].second, 3057);
+	ASSERT_EQ(distribSpd.GetTimes()[1].second, 394);
+	ASSERT_EQ(distribSpd.GetTimes()[2].second, 14);
 }
